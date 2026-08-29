ecfb32d507e2b6b98526ca46a12df3673892fee6
MatheusR30 <106620379+MatheusR30@users.noreply.github.com>
2026-08-29 15:09:11 -0300

    Delete devquest2.0/modulo-typescript/index.js

--- devquest2.0/modulo-typescript/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function sendMessage(userName, isLogged) {
-    console.log(`Olá ${userName}. Você ${!isLogged ? " não" : ""} pode logar.`);
-}
-const userName = "Beto";
-const isLogged = true;
-sendMessage(userName, isLogged);
