d74f5bb45fd096bc7c8830c9815aa925a6ae0cd1
MatheusR30 <106620379+MatheusR30@users.noreply.github.com>
2026-08-29 14:33:11 -0300

    Os arquivos complementares da aula 01 variaveis

---
+++ devquest2.0/modulo-fundamentos-js/01-variaveis-console-literal/template-literal.js
@@ -0,0 +1,8 @@
+let nome = "Matheus";
+let sobrenome = "Rodrigues";
+
+// const nomeCompleto = nome + " " + sobrenome;
+// console.log("Seja bem-vindo, " + nomeCompleto);
+
+//A forma mais elegante e atualizada de usar. TEMPLATE LITERAL.
+console.log(`Seja bem-vindo, ${nome} ${sobrenome}`);
---
+++ devquest2.0/modulo-fundamentos-js/01-variaveis-console-literal/tipos-de-variaveis.js
@@ -0,0 +1,23 @@
+//  variáveis
+
+// const - o valor da variável é contante, nao pode mudar
+// const nome = "Matheus";
+// console.log(nome);
+
+// let - o valor da variável pode ser alterado
+// A diferença entre var e let é que var tem escopo global ou de função,
+// enquanto let tem escopo de bloco. Isso significa que uma variável declarada
+//  com var pode ser acessada em qualquer lugar do código, enquanto uma variável
+//  declarada com let só pode ser acessada dentro do bloco onde foi declarada.
+let nome = "Matheus";
+nome = "João";
+
+console.log(nome);
+
+// var nome = "Matheus";
+// console.log(nome);
+
+// var nome = "João";
+// console.log(nome);
+
+// loggar / mostrar
