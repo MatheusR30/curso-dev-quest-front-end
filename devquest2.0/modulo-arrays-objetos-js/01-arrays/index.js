13599f2edffd43b47c9a65262c6307a30503df0d
MatheusR30 <106620379+MatheusR30@users.noreply.github.com>
2026-08-29 14:36:13 -0300

    Add multidimensional arrays for movie ratings

---
+++ devquest2.0/modulo-arrays-objetos-js/01-arrays/index.js
@@ -0,0 +1,20 @@
+// const filmes = ["Homem-Aranha", "Batman", "Superman", "Mulher-Maravilha"];
+
+// console.log(filmes[2]);
+
+// const array = ["Matheus", 30, { cidade: "São Paulo" }];
+
+const arrayBidimensional = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+const arrayMultiDimensional = [
+  ["Homem-Aranha", 5],
+  ["Batman", 4.5],
+  ["Superman", 4],
+  ["Mulher-Maravilha", 4.8, ["Gal Gadot", 40]],
+];
+
+console.log(avaliacoesFilmes[3][2][0]);
